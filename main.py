ef176a532115e915e510f6cbd3a223677504d553
Tahsin Alpaydın <tahsinalpaydin@gmail.com>
2020-12-12 16:45:11 +0000

    Bug fixed.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def isVideoCompatibleYoutube(url):
 def downloadYoutube(url):
 
     youtube = pytube.YouTube(url)
-    stream = youtube.streams.get_by_itag(133)
+    stream = youtube.streams.first()
     stream.download(filename="video")
     print("Downloaded the YouTube video.")
 
@@ -60,16 +60,20 @@ async def video(ctx, url):
         if "/watch" in url:
             if isVideoCompatibleYoutube(url):
                 async with ctx.typing():
-                    downloadYoutube(url)
-                    stop = time.perf_counter()
-                    await ctx.send(content=f"Processed in {stop - start:0.1f} seconds. Sent by {ctx.message.author.mention}", file=discord.File(fp="video.mp4"))
-                    await ctx.message.delete()
-                        
-                    print("Sent the YouTube video!")
-                    destruct("video.mp4")
-                    
+
+                    try:
+                        downloadYoutube(url)
+                        stop = time.perf_counter()
+                        await ctx.send(content=f"Processed in {stop - start:0.1f} seconds. Sent by {ctx.message.author.mention}", file=discord.File(fp="video.mp4"))
+                        await ctx.message.delete()
+                            
+                        print("Sent the YouTube video!")
+                        destruct("video.mp4")
+                    except:
+                        await ctx.send("Couldn't download the video from YouTube! (Video too big)")
+                        destruct("video.mp4")
             else:
-                await ctx.send("Your video is longer than 60 seconds!")
+                await ctx.send("Your video is longer than 60 seconds!")    
         else:
             await ctx.send("This type of link isn't supported!")
 
@@ -77,13 +81,17 @@ async def video(ctx, url):
         if isVideoCompatibleYoutube(url):
             async with ctx.typing():
                                       
-                downloadYoutube(url)
-                stop = time.perf_counter()
-                await ctx.send(content=f"Processed in {stop - start:0.1f} seconds. Sent by {ctx.message.author.mention}", file=discord.File(fp="video.mp4"))
-                await ctx.message.delete()
-
-                print("Sent the YouTube video!")    
-                destruct("video.mp4")
+                try:
+                    downloadYoutube(url)
+                    stop = time.perf_counter()
+                    await ctx.send(content=f"Processed in {stop - start:0.1f} seconds. Sent by {ctx.message.author.mention}", file=discord.File(fp="video.mp4"))
+                    await ctx.message.delete()
+                            
+                    print("Sent the YouTube video!")
+                    destruct("video.mp4")
+                except:
+                    await ctx.send("Couldn't download the video from YouTube! (Video too big)")
+                    destruct("video.mp4")
         else:
             await ctx.send("Your video is longer than 60 seconds!")
     elif "reddit.com" in url:
@@ -92,8 +100,10 @@ async def video(ctx, url):
           
                 try:
                     downloadReddit(url)
+                    stop = time.perf_counter()
+
                 except:
-                    await ctx.send("Couldn't download the video from Reddit!")
+                    await ctx.send("Couldn't download the video from Reddit! (Video too big)")
                                 
                 dir = []
                 for file in os.listdir():
@@ -101,13 +111,18 @@ async def video(ctx, url):
                         dir.append(file)
                 os.rename(dir[0], "video.mp4")
                                 
-                stop = time.perf_counter()
-                await ctx.send(content=f"Processed in {stop - start:0.1f} seconds. Sent by {ctx.message.author.mention}", file=discord.File(fp="video.mp4"))
-                await ctx.message.delete()
-                            
-                print("Sent the reddit video!")
-                destruct("video.mp4")
+               
+                try:
+                    await ctx.send(content=f"Processed in {stop - start:0.1f} seconds. Sent by {ctx.message.author.mention}", file=discord.File(fp="video.mp4"))
+                    await ctx.message.delete()
                     
+                    print("Sent the reddit video!")
+                    destruct("video.mp4")
+                                    
+                except:       
+                    await ctx.send("Couldn't send the video from Reddit! (Video too big.)")
+                    destruct("video.mp4")
+                 
         else:
             await ctx.send("This type of link isn't supported!")
     else:
@@ -123,9 +138,9 @@ async def help(ctx):
     embed.add_field(name='**sv ping**', value="Views the bot latency.", inline=False)
     embed.add_field(name='**sv help**', value="Displays the help command.", inline=False)
     embed.add_field(name='**sv video <url>**', value="Downloads the video from the given link and sends it.", inline=False)
-    embed.add_field(name='**Links**', value='[Invite me!](https://discord.com/api/oauth2/authorize?client_id=783728124021702689&permissions=191488&scope=bot) - [Vote me!](https://top.gg/bot/783728124021702689)')
+    embed.add_field(name='**Links**', value='[Invite me!](https://discord.com/api/oauth2/authorize?client_id=783728124021702689&permissions=191488&scope=bot) - [Vote me!](https://top.gg/bot/783728124021702689/vote)')
     embed.set_thumbnail(url="https://i.hizliresim.com/orhNo4.png")
-    embed.set_footer(icon_url="https://i.redd.it/1i6tmwht8y261.png", text="Supported platforms: YouTube and Reddit")
+    embed.set_footer(icon_url="https://i.redd.it/1i6tmwht8y261.png", text=f"Supports YouTube and Reddit | Online in {len(bot.guilds)} servers.")
     await ctx.send(embed=embed)
 
 @bot.command(aliases=['PING', 'Ping'])
