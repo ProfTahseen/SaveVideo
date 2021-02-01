4f65887ea08d36a251c3415855b712c5b71fb1b5
Enes Tiryakioğlu <hasanenestiryakioglu@gmail.com>
2021-02-01 02:40:04 +0200

    Optimized the code and fixed bugs, enjoy!

--- downloader.py
+++ downloader.py
@@ -2,16 +2,9 @@ import pytube, os
 from redvid import Downloader
 reddit = Downloader()
 
-def checkReddit(url):
-
-    if reddit.duration < 60:
-        return False
-    else:
-        return True
-
 def checkYoutube(url):
 
-    if pytube.YouTube(url).length < 60:
+    if pytube.YouTube(url).length > 60:
         return False
     else:
         return True
@@ -19,14 +12,6 @@ def checkYoutube(url):
 def downloadYoutube(url):
 
     pytube.YouTube(url).streams.get_by_itag(18).download(filename="savevideo")
-
-def downloadReddit(url):
-
-    reddit.max_s = 7.5 * (1 << 20)
-    reddit.auto_max = True
-    reddit.log = False
-    reddit.url = url
-    reddit.download()
 
 def renameReddit(name):
 
@@ -36,3 +21,11 @@ def renameReddit(name):
             dir.append(file)
 
     os.rename(dir[0], name)
+
+def downloadReddit(url):
+    reddit.max_s = 7.5 * (1 << 20)
+    reddit.auto_max = True
+    reddit.log = False
+    reddit.max_d = 60
+    reddit.url = url
+    reddit.download()
