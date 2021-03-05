d0b8fc8d88e64a2975e7a4eb0da3a1bcf3063881
Enes Tiryakioğlu <hasanenestiryakioglu@gmail.com>
2021-03-05 21:24:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,28 @@ async def on_ready():
 @bot.command(aliases=['Video'])
 @commands.cooldown(1, 15, commands.BucketType.user)
 async def video(ctx, url):
-    if "reddit.com" and "/comments/" in url:
+    if "youtu.be" or "/watch" or "/shorts" in url:
+
+        try:
+            if downloader.checkYoutube(url, 60):
+                async with ctx.typing():
+                    downloader.downloadYoutube(url)
+                    await ctx.send(content=f"YouTube video sent by {ctx.message.author.mention}", file=discord.File(fp="savevideo.mp4"))
+                    os.remove("savevideo.mp4")
+                    await ctx.message.delete()
+                    print("Sent the YouTube video!")
+            else:
+                await ctx.send("Your video is longer than 60 seconds!\n(This is because of the Discord upload limit.)")
+                print(f"Your video is longer than 60 seconds! (YouTube)\n{url}")
         
+        except:
+            await ctx.send("Something went wrong while getting the video.")
+            os.remove("savevideo.mp4")
+            await ctx.message.delete()
+            print(f"Something went wrong while getting the video. (YouTube)\n{url}")
+
+    elif "reddit.com" and "/comments/" in url:
+
         try:
             if downloader.checkReddit(url, 60):
                 async with ctx.typing():
@@ -27,37 +47,16 @@ async def video(ctx, url):
             else:
                 await ctx.send("Your video is longer than 60 seconds!\n(This is because of the Discord upload limit.)")
                 print(f"Your video is longer than 60 seconds! (Reddit)\n{url}")
-        
+
         except:
             await ctx.send("Something went wrong while getting the video.")
             os.remove("savevideo.mp4")
             await ctx.message.delete()
             print(f"Something went wrong while getting the video. (Reddit)\n{url}")
-    
-    elif "youtu.be" or "/watch" or "/shorts" in url:
-        
-        try:      
-            if downloader.checkYoutube(url, 60):
-                async with ctx.typing():
-                    downloader.downloadYoutube(url)
-                    await ctx.send(content=f"YouTube video sent by {ctx.message.author.mention}", file=discord.File(fp="savevideo.mp4"))
-                    os.remove("savevideo.mp4")
-                    await ctx.message.delete()
-                    print("Sent the YouTube video!")            
-            else:
-                await ctx.send("Your video is longer than 60 seconds!\n(This is because of the Discord upload limit.)")
-                print(f"Your video is longer than 60 seconds! (YouTube)\n{url}")
-        
-        except:
-            await ctx.send("Something went wrong while getting the video.")
-            os.remove("savevideo.mp4")
-            await ctx.message.delete()
-            print(f"Something went wrong while getting the video. (YouTube)\n{url}")
 
 @bot.command(aliases=['Help'])
 async def help(ctx):
     embed = discord.Embed(
-
         title="SaveVideo Support",
         description="Maximum video length is 60 seconds.\nSupports YouTube and Reddit.",
         colour=discord.Color.blurple())
@@ -67,7 +66,6 @@ async def help(ctx):
     embed.add_field(name='**sv video <URL>**', value="Downloads the video from the given URL.", inline=False)
     embed.add_field(name='**Links**', value='[Invite SaveVideo](https://discord.com/api/oauth2/authorize?client_id=783728124021702689&permissions=8&scope=bot) | [Feedback Server](https://discord.gg/vNmAgsB3uV) | [Source Code](https://github.com/Tahsinalp267/SaveVideoBOT)')
     embed.set_thumbnail(url="https://i.hizliresim.com/orhNo4.png")
-
     await ctx.send(embed=embed)
 
 @bot.command(aliases=['Stats'])
